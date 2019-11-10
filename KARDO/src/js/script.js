a03a50bbc3a893eac399c4344b36114cb27a97a0
tonnyhawk93 <53153875+tonnyhawk93@users.noreply.github.com>
2019-11-10 19:34:24 +0300

    Add files via upload

--- KARDO/src/js/script.js
+++ KARDO/src/js/script.js
@@ -342,4 +342,52 @@ window.addEventListener('DOMContentLoaded', () => {
     activeMark('smi__photolitle_item');
     activeMark('about__imglitle', 'about__imgbig');
     serachAllMarks();
+    let deadline = '2020-04-14';
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60)%24),
+        days = Math.floor((t/(1000*60*60*24)))
+        ;
+        return {
+            'total' : t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = document.getElementById('timer-days'),
+            hours = document.getElementById('timer-hours'),
+            minutes = document.getElementById('timer-minutes'),
+            seconds = document.getElementById('timer-seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+    setClock('timer', deadline);
 });
